45b274b85bc97e965ba16aec19635f51cd714ae5
Mati Alvarez <matialvarez.arg@gmail.com>
2021-11-17 20:44:57 -0300

    first commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,6 +3,20 @@ const fs = require("fs");
 
 async function main() {
 
+  [owner, addr1, addr2, addr3] = await ethers.getSigners();
+  const Ruleta = await hre.ethers.getContractFactory("Ruleta");
+  const ruleta = await Ruleta.deploy();
+  await ruleta.deployed();
+
+  //Agregar fondos al contrato
+  ruleta.connect(addr1).placeBet(10000);
+
+  const data = {
+    address: ruleta.address,
+    abi: JSON.parse(ruleta.interface.format("json"))
+  };
+
+  fs.writeFileSync("frontend/src/Ruleta.json", JSON.stringify(data));
 }
 
 main()
--- test/Ruleta-test.js
+++ test/Ruleta-test.js
@@ -1,6 +1,25 @@
 const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { ethers, web3 } = require("hardhat");
 
 describe("Ruleta", function () {
-    
+    let Ruleta, ruleta
+    beforeEach(async function () {
+        [owner, addr1, addr2, addr3, addr4, addr5] = await ethers.getSigners();
+        Ruleta = await ethers.getContractFactory("Ruleta");
+        ruleta = await Ruleta.deploy();
+        await ruleta.deployed();
+        await web3.eth.sendTransaction({from: addr1.address, to: ruleta.address, value: 10000})
+        ruleta.connect(addr1).placeBet(10000);
+    })
+    it("The contract should have funds", async function () {
+        const funds = await ruleta.connect(addr1).getFunds();
+        expect(funds).to.equal(10000)
+    })
+    it("The contract should pay to the winner", async function() {
+        const funds = await ruleta.connect(addr1).getFunds();
+        expect(funds).to.equal(10000)
+        await ruleta.payWinner(150, addr2.address)
+        const funds2 = await ruleta.connect(addr1).getFunds();
+        expect(funds2).to.equal(9850)
+    })
 })
